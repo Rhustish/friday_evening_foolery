8823cf9f33ab5a283183c663200b649feebf9caf
Bhavy <bbhatnagar27@gmail.com>
2026-02-20 18:59:23 +0530

    Skeleton csoned

---
+++ csoned/client/buildClient.sh
@@ -0,0 +1,6 @@
+rm -fr /home/bhavy/csoned/client/build
+mkdir -p /home/bhavy/csoned/client/build
+
+g++ game.cpp -o /home/bhavy/csoned/client/build/game 
+
+/home/bhavy/csoned/client/build/game
---
+++ csoned/client/include/constants.h
@@ -0,0 +1,6 @@
+const int TICK_RATE = 32;
+const double TICK_INTERVAL = 1.0f/32;
+
+const double MAX_VELOCITY = 3.5f;
+const double ACCELERATION = 0.5f;
+const double FRICTION = 0.75f;
